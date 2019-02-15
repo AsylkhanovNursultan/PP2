0c5fbc8bb63eff2f64292491ecdd76dc34019666
AsylkhanovNursultan <nasylkhanov2000@mail.ru>
2019-02-16 04:04:42 +0600

    Add files via upload

---
+++ WEEK2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+
+
+
+{
+    class Program
+    {
+        public static bool inPrime(int b)
+        {
+            if ((b == 1) || (b == 0))
+                return false;
+
+            for (int i = 2; i < b; i++)
+
+            {
+                if (b % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"C:\WEEK2\TASK2\reader.txt");
+            StreamWriter writer = new StreamWriter(@"C:\WEEK2\TASK2\writer.txt");
+            string n = reader.ReadLine();
+            string[] ss = n.Split(' ');
+            foreach (string s in ss)
+            {
+                if (inPrime(int.Parse(s)))
+                    writer.Write(s + " ");
+            }
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
